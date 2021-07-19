5f43773114b0f77e9c74f20d12287659f0d2654b
ren-yc <ldfx_ryc@163.com>
2021-07-19 13:27:05 +0800

    Site updated: 2021-07-19 13:27:05

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "Astronomia",
+        artist: 'Vicetone/Tony Igy',
+        url: 'https://m801.music.126.net/20210719135001/75c9080ec4c870b36375d47b012e323d/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/8932617580/9c52/89b0/44f0/ea850d69f14ccdd8a298c57cc0987c9d.mp3',
+        cover: 'https://ren-yc.github.io/assets/img/auther.jpg',
+      }
+    ]
+});
