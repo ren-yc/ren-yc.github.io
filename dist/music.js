cd899992c297c4d5dc55e35a5c973bbd6df0cba5
ren-yc <ldfx_ryc@163.com>
2021-07-19 14:20:22 +0800

    Site updated: 2021-07-19 14:20:22

--- dist/music.js
+++ dist/music.js
@@ -6,7 +6,7 @@ const ap = new APlayer({
       {
         name: "Astronomia",
         artist: 'Vicetone/Tony Igy',
-        url: 'https://m801.music.126.net/20210719135001/75c9080ec4c870b36375d47b012e323d/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/8932617580/9c52/89b0/44f0/ea850d69f14ccdd8a298c57cc0987c9d.mp3',
+        url: 'https://ren-yc.github.io/assets/music/Astronomia.mp3',
         cover: 'https://ren-yc.github.io/assets/img/auther.jpg',
       }
     ]
