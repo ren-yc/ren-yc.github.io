012f5acab6886ceed718185a14e518dd2e6921a5
ren-yc <ldfx_ryc@163.com>
2021-07-29 14:02:00 +0800

    Site updated: 2021-07-29 14:02:00

--- assets/js/argon.js
+++ assets/js/argon.js
@@ -161,33 +161,3 @@ $(document).ready(function() {
         event.preventDefault();
     });
  });
-
- var trim_indent = true;
- var line_number = true;
- // enable highlight
- $('pre code').each(function(i, block) {
-   var texts = $(this).text().split('\n');
-	// trim indent
-	if (trim_indent){
-	 var tab = texts[0].match(/^\s{0,}/);
-	 if (tab) {
-	   var arr = [];
-	   texts.forEach(function(temp) {
-		 arr.push(temp.replace(tab, ''));
-	   });
-	   $(this).text(arr.join('\n'));
-	 }
-   }
-   // add line number
-   if (line_number) {
-	 console.log("show line number in front-end");
-	 var lines = texts.length - 1;
-	 var $numbering = $('<ul/>').addClass('pre-numbering');
-	 $(this).addClass('has-numbering').parent().append($numbering);
-	 for (i = 1; i <= lines; i++) {
-	   $numbering.append($('<li/>').text(i));
-	 }
-   }
-   // hightlight
-   hljs.highlightBlock(block);
- });
